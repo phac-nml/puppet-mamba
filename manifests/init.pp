efb7032ecce9a345bf16324cb2806898c955f765
Jacob Janzen <jjanzen@cscscience.ca>
2023-06-05 10:57:25 -0500

    use . instead of source

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,8 @@ class mamba (
     path   => "/tmp/mambaforge-${version}.sh",
     source => "https://github.com/conda-forge/miniforge/releases/download/${version}/Mambaforge-Linux-x86_64.sh"
   }
-  exec { "install mamba":
-    command  => "export HOME='/root' && bash /tmp/mambaforge-${version}.sh -b -p '/opt/conda' -f && source '/opt/conda/etc/profile.d/conda.sh' && conda activate && source '/opt/conda/etc/profile.d/mamba.sh' && mamba init --system",
+  exec { 'install mamba':
+    command  => "export HOME='/root' && bash /tmp/mambaforge-${version}.sh -b -p '/opt/conda' -f && . '/opt/conda/etc/profile.d/conda.sh' && conda activate && . '/opt/conda/etc/profile.d/mamba.sh' && mamba init --system",
     provider => 'shell',
     require  => File["/tmp/mambaforge-${version}.sh"],
     creates  => '/opt/conda/bin/mamba'
